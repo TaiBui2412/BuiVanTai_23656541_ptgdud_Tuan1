be80bcd606bde1b63c63c0782b6b797b0d729659
Bui Van Tai <buivantai24122004@gmail.com>
2026-01-18 00:38:47 +0700

    react bai 1

---
+++ react_bai1/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src="img/Product1.jpg" alt="" />
+        <h1>Product 1</h1>
+        <p>10$</p>
+        <button>Add to Cart</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
