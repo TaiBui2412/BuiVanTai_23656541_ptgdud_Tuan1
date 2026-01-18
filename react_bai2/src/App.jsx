cd1ab8fea901455649047c6e10f2773858ff03ee
Bui Van Tai <buivantai24122004@gmail.com>
2026-01-18 12:05:15 +0700

    react bai 2

---
+++ react_bai2/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from './Button'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button type="primary">Primary</Button>
+        <Button type="danger">Delete</Button>
+        <Button type="success">Save</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
